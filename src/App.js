7241e4b30671f3ec5a52928b73f58a062426d227
andrew-mcintosh <andrewdavidmcintosh@gmail.com>
2019-09-25 14:17:25 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.scss";
+import NavBar from "./components/NavBar/NavBar";
+import TourList from "./components/TourList/TourList";
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <TourList />
+      </React.Fragment>
+    );
+  }
+}
+export default App;
